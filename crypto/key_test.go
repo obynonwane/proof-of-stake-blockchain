a910458ff56dd178eeb9be30e0a25f0c45fbe489
obynonwane <biostechnologyng@gmail.com>
2024-07-04 17:06:28 +0100

    finished testing file for generating account address from string

--- crypto/key_test.go
+++ crypto/key_test.go
@@ -18,6 +18,21 @@ func TestGeneratePrivateKey(t *testing.T) {
 	assert.Equal(t, len(pubKey.Byte()), pubKeyLen)
 }
 
+func TestPrivateKeyFromString(t *testing.T) {
+
+	var (
+		seed       = "447e9e133c4c17155ecb739388e06b80d8444d70d5ff43541f6cdf43ab13cf61"
+		addressStr = "3505405aa008c9b5bac40f5f88254299e61cf13f"
+		privKey    = NewPrivateKeyFromString(seed)
+	)
+	assert.Equal(t, privKeyLen, len(privKey.Bytes()))
+	address := privKey.Public().Address()
+	assert.Equal(t, addressStr, address.String())
+
+	// io.ReadFull(rand.Reader, seed)
+	// fmt.Println(hex.EncodeToString(seed))
+}
+
 func TestPrivateKeySign(t *testing.T) {
 	//create private key
 	privKey := GeneratePrivateKey()
